da14d5f272d5bc7177cab22b8226b9fb893c26ae
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-04-07 16:51:23 -0400

    Upload avatar users, and finally chapter 3

--- TrilhaNode/RentalX/src/middlewares/ensureAuthenticated.ts
+++ TrilhaNode/RentalX/src/middlewares/ensureAuthenticated.ts
@@ -33,6 +33,8 @@ export default async function ensureAuthenticated(
       throw new AppError('User does not exists', 401);
     }
 
+    request.user = { id: user_id };
+
     next();
   } catch {
     throw new AppError('invalid token', 401);
